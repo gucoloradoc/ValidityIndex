50f2acb3046b38d908e6fba1aafa3eb4fdb53d29
Gerson Colorado <gucoloradoc@unal.edu.co>
2019-09-26 16:52:00 -0500

    Edited visualization and README

--- code/visualizations.py
+++ code/visualizations.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 base_df= pd.read_csv('data/all_data.csv')
 base_df['attribute']=base_df['attribute'].astype('category')
@@ -8,13 +9,13 @@ unive_ranking=pd.read_csv('data/search_keys2.csv')
 
 ##General distributions, all data
 attributes=base_df['attribute'].cat.categories.values
-for attribute in attributes:
-    attribute_base=base_df[base_df['attribute']==attribute]
-    ax = np.log(attribute_base.pivot(columns='year', values='count')).plot.hist(alpha=0.5,bins=50)
-    ax.set_xlabel('ln(Documents count)')
-    ax.set_ylabel('Frequency')
-    ax.set_title('Document count distribution for ' +attribute)
-    ax.figure.savefig('images/exploratory/'+attribute+'_log.pdf')
+#for attribute in attributes:
+#    attribute_base=base_df[base_df['attribute']==attribute]
+#    ax = np.log(attribute_base.pivot(columns='year', values='count')).plot.hist(alpha=0.5,bins=50)
+#    ax.set_xlabel('ln(Documents count)')
+#    ax.set_ylabel('Frequency')
+#    ax.set_title('Document count distribution for ' +attribute)
+#    ax.figure.savefig('images/exploratory/'+attribute+'_log.pdf')
 
 ## Plotting the results of VIC
 with open("output/results_VIC_example.list",'rb') as file:
