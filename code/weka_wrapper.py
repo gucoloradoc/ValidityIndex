42ff226747e8e7e21cfaa6d918ae978807ae046d
Gerson Colorado <gucoloradoc@unal.edu.co>
2019-09-29 04:09:16 -0500

    Bayes_NEtwork working

--- code/weka_wrapper.py
+++ code/weka_wrapper.py
@@ -1,20 +1,21 @@
 import weka.core.jvm as jvm
 from weka.core.converters import Loader
 from weka.filters import Filter
-jvm.start() #Remember to initialize this
+jvm.start(system_cp=True, packages=True, max_heap_size="512m") #Remember to initialize this CLASSPATH env var
 
 #Preparing the data
 loader = Loader(classname="weka.core.converters.ArffLoader")
-data = loader.load_file('data/datatobayes.arff')
-#remove = Filter(classname="weka.filters.unsupervised.attribute.Remove", options=["-R", "first"])
-#remove.inputformat(data)
-filtered=data #remove.filter(data)
+#data = loader.load_file('data/datatobayes.arff')
+data = loader.load_file('data/Full.arff')
+remove = Filter(classname="weka.filters.unsupervised.attribute.Remove", options=["-R", "first"])
+remove.inputformat(data)
+filtered= remove.filter(data)
 
 #Classifier test
 from weka.classifiers import Classifier, Evaluation
 from weka.core.classes import Random
 filtered.class_is_last()
-classifier = Classifier(classname="weka.classifiers.bayes.net") #options=["-C", "0.3"]
+classifier = Classifier(classname="weka.classifiers.bayes.BayesNet", options=['-D']) #
 evaluation = Evaluation(filtered)
 evaluation.crossvalidate_model(classifier, filtered, 10, Random(42))
 evaluation.area_under_roc(class_index=0) #ROC
